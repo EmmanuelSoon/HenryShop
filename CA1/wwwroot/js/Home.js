dcc585d36f8eb90adcf61fb798d8e9c93a3e8102
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-04-12 15:24:50 +0800

    typo error

--- CA1/wwwroot/js/Home.js
+++ CA1/wwwroot/js/Home.js
@@ -44,7 +44,7 @@ function AddToCart(id) {
                         '</p>' +
                         '      </div>' +
                         '      <div class="modal-footer">' +
-                        '        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+                        '        <button type="button" class="btn btn-default" data-dismiss="modal">Continue Shopping</button>' +
                         '      </div>' +
                         '    </div>' +
                         '  </div>' +
