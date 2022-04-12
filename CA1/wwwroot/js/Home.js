2470f1e2efefa8b3d17f8c56239005d01d0d7877
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-04-12 15:15:08 +0800

    use a modal instead of alert to show adding to cart

--- CA1/wwwroot/js/Home.js
+++ CA1/wwwroot/js/Home.js
@@ -31,7 +31,27 @@ function AddToCart(id) {
 
 
                 if (data.status == "success") {
-                    alert(data.name + " Added to cart");
+                    $('body').append(
+                        '<div></div><div id="myModal" class="modal fade" role="dialog">' +
+                        '  <div class="modal-dialog">' +
+                        '    <div class="modal-content">' +
+                        '      <div class="modal-header">' +
+                        '        <h3 class="modal-title">Success!</h3>' +
+                        '        <button type = "button" class= "close" data-dismiss="modal">&times;</button>' +
+                        '      </div>' +
+                        '      <div class="modal-body">' +
+                        '           <p>' + data.name + ' Added to cart' +
+                        '</p>' +
+                        '      </div>' +
+                        '      <div class="modal-footer">' +
+                        '        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+                        '      </div>' +
+                        '    </div>' +
+                        '  </div>' +
+                        '</div>');
+                    $('#myModal').modal('show');
+                    
+                    //alert(data.name + " Added to cart");
                 }
             }
         }
