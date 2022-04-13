f270ab909f83c9f22061b5f71ada8b95738ec520
Youcheng Li <e0941660@u.nus.edu>
2022-04-13 15:11:16 +0800

    Added Product

---
+++ CA1/Models/ProductReview.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CA1.Models
+{
+    public class ProductReview
+    {
+        public ProductReview()
+        {
+            Id = new Guid();
+            CreatedDate = DateTime.Today.ToString("d");
+        }
+        public Guid Id { get; set; }
+        public virtual Guid OrderId { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Content { get; set; }
+        [Range(0, 5)]
+        public int Rating { get; set; }
+        public string CreatedDate { get; set; }
+    }
+}
