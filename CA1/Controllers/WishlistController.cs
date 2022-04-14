23ce0d9cc33423d16afe3b1c5d30ef58ecdc8a0b
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-04-14 12:14:44 +0800

    wishlist view and controller

---
+++ CA1/Controllers/WishlistController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CA1.Controllers
+{
+    public class WishlistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
