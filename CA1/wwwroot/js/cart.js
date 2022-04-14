def41ce8b806dd53d0639eb884a1b42286468735
Anandeeswaran Venkatachalam <e0941664@u.nus.edu>
2022-04-14 18:08:10 +0800

    Bug/Functionality  - > Edit quantity through textbox as well

--- CA1/wwwroot/js/cart.js
+++ CA1/wwwroot/js/cart.js
@@ -1,104 +1,23 @@
 ﻿window.onload = function () {
-    let adding = document.getElementsByClassName("btn-outline-success");
-    let minus = document.getElementsByClassName("btn-outline-danger");
     let remove = document.getElementsByClassName("btn-danger");
+    let quantity = document.getElementsByClassName("btn-secondary");
 
-    //for (let i = 0; i < adding.length; i++) {
-    //    adding[i].addEventListener('click', Addclick);
-    //}
-
-    //for (let i = 0; i < minus.length; i++) {
-    //    minus[i].addEventListener('click', Minusclick);
-    //}
-
-    //for (let i = 0; i < remove.length; i++) {
-    //    remove[i].addEventListener('click', Removeclick);
-    //}
-
-    // combine above three codes
-    for (let i = 0; i < adding.length; i++) {
-        adding[i].addEventListener('click', Addclick);
-        minus[i].addEventListener('click', Minusclick);
+    for (let i = 0; i < remove.length; i++) {
         remove[i].addEventListener('click', Removeclick);
     }
 
-    function Addclick(event) {
-        PlusToCart(event.target.value);
-    }
-
-    function Minusclick(event) {
-        if (event.target.getAttribute('data-value') <= 0) {
-            RemoveFromCart(event.target.getAttribute('value'));
-        }
-        else {
-            MinusFromCart(event.target.getAttribute('value'));
-        }
+    for (let i = 0; i < quantity.length; i++) {
+        quantity[i].addEventListener('input', UpdateQuantityCart);
     }
 
     function Removeclick(event) {
-        RemoveFromCart(event.target.value);       
+        RemoveFromCart(event.target.value);
     }
 
     //modal stuff here:
     let elem = document.getElementById("checkoutBtn");
     elem.addEventListener('click', ShowModal);
 }
-
-
-
-
-
-function PlusToCart(id) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open("POST", "/CheckOut/PlusToCart");
-
-    xhr.setRequestHeader("Content-Type", "application/json; charset=utf8");
-
-    xhr.onreadystatechange = function () {
-        if (this.readyState == XMLHttpRequest.DONE) {
-            if (this.status == 200) {
-                // convert server's JSON string to a JavaScript object
-                let data = JSON.parse(this.responseText);
-
-
-                if (data.status == "success") {
-                    window.location.reload(true);
-                }
-            }
-        }
-    }
-
-    let req = { "Id": id }
-
-    xhr.send(JSON.stringify(req));
-}
-
-function MinusFromCart(id) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open("POST", "/CheckOut/MinusFromCart");
-
-    xhr.setRequestHeader("Content-Type", "application/json; charset=utf8");
-
-    xhr.onreadystatechange = function () {
-        if (this.readyState == XMLHttpRequest.DONE) {
-            if (this.status == 200) {
-                let data = JSON.parse(this.responseText);
-
-
-                if (data.status == "success") {
-                    window.location.reload(true);
-                }
-            }
-        }
-    }
-
-    let req = { "Id": id }
-
-    xhr.send(JSON.stringify(req));
-}
-
 
 function RemoveFromCart(id) {
     if (confirm("Remove item from Cart?")) {
@@ -127,8 +46,56 @@ function RemoveFromCart(id) {
     }
 }
 
-//show login modal
+function UpdateQuantityCart(event) {
+    let productId = event.target.id;
+    let shopcartitemid = event.target.getAttribute('data-value');
+    let ProductpriceId = "ProductPrice" + productId;
 
+    let value = document.getElementById(productId).value * 1;
+
+    let Productprice = parseFloat(document.getElementById(ProductpriceId).innerHTML.substring(1));
+
+    if (value < 1 || !Number.isInteger(value)) {
+        alert("Please input a correct quantity. You may remove the item by clicking on the delete icon on the right.");
+        document.getElementById(productId).value = 1;
+    }
+
+    value = document.getElementById(productId).value * 1;
+
+    UpdateCartItemwithQuantity(productId, value, shopcartitemid);
+}
+
+function UpdateCartItemwithQuantity(productId, value, shopcartitemid) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "/CheckOut/UpdateQuantity");
+
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf8");
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE) {
+            if (this.status == 200) {
+                let data = JSON.parse(this.responseText);
+
+
+                if (data.status == "success") {
+                    window.location.reload(true);
+                }
+            }
+        }
+    }
+
+    let shopcartitem = {
+        Id: shopcartitemid,
+        ProductId: productId,
+        Quantity: value
+
+    }
+
+    xhr.send(JSON.stringify(shopcartitem));
+}
+
+//show login modal
 function ShowModal(event) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "/Checkout/CheckUser");
