e4a5749113ce1b1a6eee227a887cc63773cbde73
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-04-14 14:36:37 +0800

    wishlist view and controller done. (testing needs to be done after addtowishlist function created)

--- CA1/Controllers/WishlistController.cs
+++ CA1/Controllers/WishlistController.cs
@@ -1,11 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using CA1.Models;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
 
 namespace CA1.Controllers
 {
     public class WishlistController : Controller
     {
+        private DBContext dbContext;
+
+        public WishlistController(DBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
+            User user = dbContext.Users.FirstOrDefault(x => (Request.Cookies["SessionId"] != null) && (x.sessionId == Guid.Parse(Request.Cookies["SessionId"])));
+            if(user == null)
+            {
+                return RedirectToAction("Index", "LogIn");
+            }
+            else
+            {
+                WishList wishList = dbContext.WishLists.FirstOrDefault(x => x.Id == user.Id);
+                List<WishListItem> wishListItems = dbContext.WishListItems.Where(x => x.WishListId == wishList.Id).ToList();
+                List<int> stockcount = new List<int>();
+
+                foreach(WishListItem item in wishListItems)
+                {
+                    List<InventoryRecord> record = dbContext.InventoryRecords.Where(x => x.ProductId == item.ProductId).ToList();
+                    if(record.Count > 0)
+                    {
+                        stockcount.Add(record.Count);
+                    }
+                    else
+                    {
+                        stockcount.Add(0);
+                    }
+                }
+
+                ViewBag.WishList = wishListItems;
+                ViewBag.StockCount = stockcount;
+            }
+
             return View();
         }
     }
